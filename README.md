9017b2ec083a8e729152bed1eb3a5648862ce064
Mark Hodgkinson <m.hodgkinson@sheffield.ac.uk>
2020-05-07 16:32:47 +0100

    Add first set of files. Update README with compiler instructions.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # RayTracing
 Repo to learn ray tracing on CPU and GPU
+
+#Instructions
+#You can compile this with gcc, but will need at least c++ 17
+g++ -std=c++17 main.cc
---
+++ main.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+
+#include "vec3.h"
+#include "color.h"
+#include "ray.h"
+
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = r.origin() - center;
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2.0 * dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius*radius;
+    auto discriminant = b*b - 4*a*c;
+    return (discriminant > 0);
+}
+
+
+color ray_color(const ray& r) {
+    if (hit_sphere(point3(0,0,-1), 0.5, r))
+        return color(1, 0, 0);
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
+}
+
+int main() {
+    const double aspect_ratio = 16.0 / 9.0;
+    const int image_width = 384;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    point3 origin(0.0, 0.0, 0.0);
+    vec3 horizontal(4.0, 0.0, 0.0);
+    vec3 vertical(0.0, 2.0, 0.0);
+    point3 lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0,0,1);
+
+
+
+    for (int j = image_height-1; j >= 0; --j) {
+        for (int i = 0; i < image_width; ++i) {
+          auto u = double(i) / (image_width-1);
+          auto v = double(j) / (image_height-1);
+          ray r(origin, lower_left_corner + u*horizontal + v*vertical);
+          color pixel_color = ray_color(r);
+          write_color(std::cout, pixel_color);
+        }
+    }
+    std::cerr << "\nDone.\n";
+}
