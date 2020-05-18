a91f6b52ee2e9339b88221e72440e04a85769758
Mark Hodgkinson <m.hodgkinson@sheffield.ac.uk>
2020-05-18 16:53:50 +0100

    Migrate main.cu to use vector class. Migrate vector class to be suitable for cuda, and drop aliases nomenclenture back to standard C++ (e.g c++17 is not supported by nvidia).

--- main.cu
+++ main.cu
@@ -4,6 +4,8 @@
 #include <fstream>
 #include <iostream>
 
+#include "vec3.h"
+
 #define checkCudaErrors(val) check_cuda( (val), #val, __FILE__, __LINE__ )
 void check_cuda(cudaError_t result, char const *const func, const char *const file, int const line) {
     if (result) {
@@ -15,14 +17,15 @@ void check_cuda(cudaError_t result, char const *const func, const char *const fi
     }
 }
 
-__global__ void render(double* fb, int* max_x, int* max_y){
+__global__ void render(vec3* fb_vec3, int* max_x, int* max_y){
   int i = threadIdx.x + blockDim.x * blockIdx.x;
   int j = threadIdx.y + blockDim.y * blockIdx.y;
   if (i >= *max_x || j >= *max_y) return;
-  int pixelIndex = j* (*max_x)*3 + i*3;
-  fb[pixelIndex] = (float)i/(*max_x -1);
-  fb[pixelIndex + 1] = (float)j/(*max_y -1);
-  fb[pixelIndex + 2] = 0.25;
+  int pixelIndex = j* (*max_x) + i;
+  double r = (double)i/(*max_x -1);
+  double g = (double)j/(*max_y -1);
+  double b = 0.25;
+  fb_vec3[pixelIndex] = vec3(r,g,b);
 }
 
 int main(){
@@ -37,14 +40,14 @@ int main(){
   *ny_cpu = 256;
 
   int num_pixels = *nx_cpu * (*ny_cpu);
-  size_t fb_size = 3*num_pixels*sizeof(double);
 
   //allocates memory on the CPU
-  double* fb_cpu = (double*)malloc(fb_size);
+  size_t fb_vec3_size = num_pixels*sizeof(vec3);
+  vec3* fb_vec3_cpu = (vec3*)malloc(fb_vec3_size);
 
   //allocates memory on the GPU, first argument is a pointer to a pointer to that memory
-  double *fb_gpu;
-  checkCudaErrors(cudaMalloc((void **)&fb_gpu, fb_size));
+  vec3* fb_vec3_gpu;
+  checkCudaErrors(cudaMalloc((void **)&fb_vec3_gpu, fb_vec3_size));
 
   int *nx_gpu;
   checkCudaErrors(cudaMalloc((void**)&nx_gpu, sizeof(int)));
@@ -56,25 +59,20 @@ int main(){
 
   dim3 blocks(*nx_cpu/tx+1,*ny_cpu/ty+1);
   dim3 threads(tx,ty);
-
-  render<<<blocks,threads>>>(fb_gpu,nx_gpu,ny_gpu);
+  render<<<blocks,threads>>>(fb_vec3_gpu,nx_gpu,ny_gpu);
   cudaDeviceSynchronize();
-  checkCudaErrors(cudaMemcpy(fb_cpu,fb_gpu,fb_size,cudaMemcpyDeviceToHost));
+  checkCudaErrors(cudaMemcpy(fb_vec3_cpu,fb_vec3_gpu,fb_vec3_size,cudaMemcpyDeviceToHost));
 
   std::cout << "P3\n" << *nx_cpu << ' ' << *ny_cpu << "\n255\n";
 
   for (int j = *ny_cpu-1; j >= 0; --j) {
     for (int i = 0; i < *nx_cpu; ++i) {
-      int pixelIndex = j* (*nx_cpu)*3 + i*3;
-      double r = fb_cpu[pixelIndex];
-      double g = fb_cpu[pixelIndex + 1];
-      double b = fb_cpu[pixelIndex + 2];
-
-      int ir = static_cast<int>(255.999 * r);
-      int ig = static_cast<int>(255.999 * g);
-      int ib = static_cast<int>(255.999 * b);
+      int pixelIndex = j* (*nx_cpu) + i;
+      vec3 pixel = fb_vec3_cpu[pixelIndex];
+      int ir = static_cast<int>(255.999 * pixel.x());
+      int ig = static_cast<int>(255.999 * pixel.y());
+      int ib = static_cast<int>(255.999 * pixel.z());
       std::cout << ir << ' ' << ig << ' ' << ib << '\n';
-
     }
   }
 
--- vec3.h
+++ vec3.h
@@ -8,40 +8,40 @@ using std::sqrt;
 
 class vec3 {
     public:
-        vec3() { e[0] = 0.0; e[1] = 0.0; e[2] = 0.0; }
-        vec3(double e0, double e1, double e2)  { e[0] = e0; e[1] = e1; e[2] = e2;};
+        __host__ __device__ vec3() { e[0] = 0.0; e[1] = 0.0; e[2] = 0.0; }
+        __host__ __device__ vec3(double e0, double e1, double e2)  { e[0] = e0; e[1] = e1; e[2] = e2;};
 
-        double x() const { return e[0]; }
-        double y() const { return e[1]; }
-        double z() const { return e[2]; }
+        __host__ __device__ double x() const { return e[0]; }
+        __host__ __device__ double y() const { return e[1]; }
+        __host__ __device__ double z() const { return e[2]; }
 
-        vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
-        double operator[](int i) const { return e[i]; }
-        double& operator[](int i) { return e[i]; }
+        __host__ __device__ vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
+        __host__ __device__ double operator[](int i) const { return e[i]; }
+        __host__ __device__ double& operator[](int i) { return e[i]; }
 
-        vec3& operator+=(const vec3 &v) {
+        __host__ __device__ vec3& operator+=(const vec3 &v) {
             e[0] += v.e[0];
             e[1] += v.e[1];
             e[2] += v.e[2];
             return *this;
         }
 
-        vec3& operator*=(const double t) {
+        __host__ __device__ vec3& operator*=(const double t) {
             e[0] *= t;
             e[1] *= t;
             e[2] *= t;
             return *this;
         }
 
-        vec3& operator/=(const double t) {
+        __host__ __device__ vec3& operator/=(const double t) {
             return *this *= 1/t;
         }
 
-        double length() const {
+        __host__ __device__ double length() const {
             return sqrt(length_squared());
         }
 
-        double length_squared() const {
+        __host__ __device__ double length_squared() const {
             return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
         }
 
@@ -50,8 +50,8 @@ class vec3 {
 };
 
 // Type aliases for vec3
-using point3 = vec3;   // 3D point
-using color = vec3;    // RGB color
+typedef vec3 point3;   // 3D point
+typedef vec3 color;    // RGB color
 
 // vec3 Utility Functions
 
@@ -59,43 +59,43 @@ inline std::ostream& operator<<(std::ostream &out, const vec3 &v) {
     return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
 }
 
-inline vec3 operator+(const vec3 &u, const vec3 &v) {
+__host__ __device__ inline vec3 operator+(const vec3 &u, const vec3 &v) {
     return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
 }
 
-inline vec3 operator-(const vec3 &u, const vec3 &v) {
+__host__ __device__ inline vec3 operator-(const vec3 &u, const vec3 &v) {
     return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
 }
 
-inline vec3 operator*(const vec3 &u, const vec3 &v) {
+__host__ __device__ inline vec3 operator*(const vec3 &u, const vec3 &v) {
     return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
 }
 
-inline vec3 operator*(double t, const vec3 &v) {
+__host__ __device__ inline vec3 operator*(double t, const vec3 &v) {
     return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
 }
 
-inline vec3 operator*(const vec3 &v, double t) {
+__host__ __device__ inline vec3 operator*(const vec3 &v, double t) {
     return t * v;
 }
 
-inline vec3 operator/(vec3 v, double t) {
+__host__ __device__ inline vec3 operator/(vec3 v, double t) {
     return (1/t) * v;
 }
 
-inline double dot(const vec3 &u, const vec3 &v) {
+__host__ __device__ inline double dot(const vec3 &u, const vec3 &v) {
     return u.e[0] * v.e[0]
          + u.e[1] * v.e[1]
          + u.e[2] * v.e[2];
 }
 
-inline vec3 cross(const vec3 &u, const vec3 &v) {
+__host__ __device__ inline vec3 cross(const vec3 &u, const vec3 &v) {
     return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
                 u.e[2] * v.e[0] - u.e[0] * v.e[2],
                 u.e[0] * v.e[1] - u.e[1] * v.e[0]);
 }
 
-inline vec3 unit_vector(vec3 v) {
+__host__ __device__ inline vec3 unit_vector(vec3 v) {
     return v / v.length();
 }
 
