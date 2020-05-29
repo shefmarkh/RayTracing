9ef3c4f964dee1c4e91a105b522ea41ba3cefafc
Mark Hodgkinson <m.hodgkinson@sheffield.ac.uk>
2020-05-29 16:10:29 +0100

    Update common functions to split into more functions.

--- CommonFunctions.h
+++ CommonFunctions.h
@@ -1,17 +1,47 @@
 #ifndef COMMONFUNCTIONS_H
 #define COMMONFUNCTIONS_H
 
+#include <vector>
+
 #ifdef __CUDACC__
 __device__ 
 #endif 
-bool hit_sphere(const point3& center, double radius, const ray& r) {
+void calcDiscriminant(const point3& center, double radius, const ray& r, double (&discriminant)[3]){
   vec3 oc = r.origin() - center;
   double a = dot(r.direction(), r.direction());
   double b = 2.0 * dot(oc, r.direction());
   double c = dot(oc, oc) - radius*radius;
-  double discriminant = b*b - 4*a*c;
-  return (discriminant > 0);
+  discriminant[0] = b*b - 4*a*c;
+  discriminant[1] = a;
+  discriminant[2] = b;
 }
+
+#ifdef __CUDACC__
+__device__ 
+#endif 
+bool hit_sphere(const point3& center, double radius, const ray& r) {  
+  const unsigned int arraySize = 3;
+  double discriminant[arraySize];
+  for (unsigned int count = 0; count < arraySize; count++) discriminant[count] = 0.0;
+  calcDiscriminant(center,radius,r,discriminant);
+  return (discriminant[0] > 0);
+}
+
+#ifdef __CUDACC__
+__device__ 
+#endif 
+double calcHitDisciminantOnSphere(const point3& center, double radius, const ray& r) {
+  const unsigned int arraySize = 3;
+  double discriminant[arraySize];
+  for (unsigned int count = 0; count < arraySize; count++) discriminant[count] = 0.0;
+  calcDiscriminant(center,radius,r,discriminant);
+  if (discriminant[0] < 0) {
+    return -1.0;
+  } else {
+    return (-discriminant[2] - sqrt(discriminant[0]) ) / (2.0*discriminant[1]);
+  }
+}
+
 #ifdef __CUDACC__
 __device__ 
 #endif 
